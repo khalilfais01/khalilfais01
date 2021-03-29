b177bde2011fd7438963fe77c91b6487eb90fa7d
khalilfais01 <73222827+khalilfais01@users.noreply.github.com>
2021-03-29 18:28:07 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+if sys.platform=='linux':
+    os.system('clear')
+    print('Linux')
+    
+else:
+    os.system('cls')
+    print('Windows')
+    
+import numpy as np
+
+np()
